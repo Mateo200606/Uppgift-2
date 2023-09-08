507f8f2e3c49563019de06c3dd61c49d69188185
Admin <Admin@TEB123011>
2023-09-08 15:03:35 +0200

    Add project files.

---
+++ Uppgift-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Uppgift2
+{
+    
+   class program
+
+   {
+        static void Main(string[] args) 
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.White;
+           
+            Console.WriteLine("Datorn har kraschat stäng av och sett på datorn för att komma in igen");
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+   }
+
+}
+
